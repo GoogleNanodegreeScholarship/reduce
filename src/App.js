34fc142c758fbc98783e9d68e0a4c15bc5465d97
americanman <snowboardracer7@gmail.com>
2018-12-24 17:25:20 -0500

    Add reducer predefined function

--- src/App.js
+++ src/App.js
@@ -8,15 +8,20 @@ class App extends Component {
       textAlign: 'left'
     }
     const array = [ 1, 2, 3, 4, 5 ]
+    //defined reducer function
+    const reducer = (accumulator, currVal) => accumulator + currVal
     return (
       <div className="App">
-        <h1>REDUCE function</h1>
+        <h1>REDUCE callback function</h1>
         <ul style={{width: '50%'}}>
           <li style={liStyle}>our array = [ 1, 2, 3, 4, 5 ]</li>
           <li style={liStyle}>To get total value of the array we use reduce method</li>
           <li style={liStyle}>.reduce() method take a callback function with <br/>two arguments(reducer(or accumulataor) and current value)</li>
-          <li style={liStyle}>array.reduce((acc, currentVal) => acc + cV) = {array.reduce((acc, cV) => acc + cV)}</li>
+          <li style={liStyle}>array.reduce((acc, cV) => acc + cV) = {array.reduce((acc, cV) => acc + cV)}</li>
         </ul>
+        <hr/>
+        <h1>REDUCE defined function</h1>
+        {array.reduce(reducer, 5)}
       </div>
     );
   }
