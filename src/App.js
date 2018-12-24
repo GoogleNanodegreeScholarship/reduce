2fbd1ca238e08472e8a9ac98030dea68b073b798
americanman <snowboardracer7@gmail.com>
2018-12-23 19:56:37 -0500

    Init commit

--- src/App.js
+++ src/App.js
@@ -6,20 +6,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>REDUCE function</h1>
+        <p>our array = [ 1, 2, 3, 4, 5 ]</p>
+        <p>To get total value of the array we use reduce method</p>
       </div>
     );
   }
