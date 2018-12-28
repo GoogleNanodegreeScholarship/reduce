203cadb177ca1f74eec0a7d700c2d2e22ae0e544
americanman <snowboardracer7@gmail.com>
2018-12-28 12:47:36 -0500

    Add initialValue to callback function

--- src/App.js
+++ src/App.js
@@ -18,9 +18,9 @@ class App extends Component {
           <li style={liStyle}>.reduce() method take a callback function with <br/>two arguments(reducer(or accumulataor) and current value)</li>
           <li style={liStyle}>array.reduce((acc, cV) => acc + cV) = {array.reduce((acc, cV) => acc + cV)}</li>
         </ul>
-          <h3>To add initialValue to call back funciton</h3>
-          <p>array.reduce((acc, cV) => acc + cV, initialVal)</p>
-          <p>array.reduce((acc, cV) => acc + cV, 10) = {array.reduce((acc, cV) => acc + cV, 10)}</p>
+        <h3>To add initialValue to call back funciton</h3>
+        <p>array.reduce((acc, cV) => acc + cV, initialVal)</p>
+        <p>array.reduce((acc, cV) => acc + cV, 10) = {array.reduce((acc, cV) => acc + cV, 10)}</p>
         <hr/>
         <h1>REDUCE pre defined function</h1>
         <p>array.reduce(reducer) = {array.reduce(reducer)}</p>
